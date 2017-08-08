b4913fb6d57d86453df424e2955a8c1f4f10b320
490456661@qq.com <490456661@qq.com>
2017-08-08 22:04:59 +0800

    提交

--- src/main/java/com/onway/web/controller/DemoApplication.java
+++ src/main/java/com/onway/web/controller/DemoApplication.java
@@ -13,6 +13,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,7 @@ import java.util.List;
  */
 @SpringBootApplication
 @ComponentScan("com.onway.web.**")
+@ServletComponentScan("com.onway.web.**")
 @EnableAutoConfiguration
 @EnableConfigurationProperties({Config.class})
 @RestController
